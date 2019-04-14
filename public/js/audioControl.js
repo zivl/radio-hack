d0400e05114f576954948daa558d766e3ee6a166
Assaf Ganor <assafg@wix.com>
2019-04-14 22:09:31 +0300

    play button

---
+++ public/js/audioControl.js
@@ -0,0 +1,49 @@
+const audioPlayer = document.querySelector('audio');
+const circleText = document.querySelector('#circle textPath');
+const svg = document.querySelector('.icon-play');
+const svgPath = svg.querySelector('path');
+const circleTextOptions = {play: "Play Broadcast. Play Broadcast. Play Broadcast. ", pause: "Pause Broadcast. Pause Broadcast. Pause Broadcast. ", offline: "Pause Broadcast. Pause Broadcast. Pause Broadcast. "};
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event) {
+  // const data = JSON.parse(event.data);
+  const data = event.data;
+  switch (data.type) {
+    case 'VOLUME_CHANGE':
+      audioPlayer.volume = data.volume / 100;
+      break;
+    case 'TOGGLE_MUTE':
+      handleMute();
+      break;
+  }
+}
+
+function handlePlay() {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    circleText.innerHTML = circleTextOptions.play;
+  } else {
+    audioPlayer.pause();
+    circleText.innerHTML = circleTextOptions.pause;
+  }
+  togglePlay();
+}
+
+function handleMute() {
+  audioPlayer.muted = !audioPlayer.muted
+}
+
+function togglePlay() {
+  if( svg.classList.contains('play') ) {
+    svg.classList.remove('play');
+    TweenLite.to(svgPath, .4, {
+    attr: { d: svg.getAttribute('data-play')}
+    });
+  }
+  else {
+    svg.classList.add('play');
+    TweenLite.to(svgPath, .4, {
+    attr: {d: svg.getAttribute('data-pause')}
+    });
+  }
+}
